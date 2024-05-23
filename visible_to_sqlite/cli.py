7e7435339d98c9476e43ceeeb376a4e5e9ecb8b4
Avner Shanan <avner@hey.com>
2024-05-23 16:29:35 -0500

    Read from CSV at the path passed in

--- visible_to_sqlite/cli.py
+++ visible_to_sqlite/cli.py
@@ -1,10 +1,24 @@
 import click
+import csv
 import os
 import sqlite_utils
 
 @click.command()
-def test123():
-	print('hello')
+@click.argument(
+    "export_file",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, allow_dash=False),
+    required=True,
+)
+def cli(export_file):
+    "Convert exported CSV from Visible app to a SQLite DB"
+    db_path = "./visible.db"
+    db = sqlite_utils.Database(db_path)
+    click.echo('opening csv')
+    with open(export_file, newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            click.echo('row')
+            click.echo(row)
 
 if __name__ == '__main__':
-	test123()
+    cli()
