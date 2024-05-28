c5de0371bc71e2b25fb545058cf17e12367dcee7
Avner Shanan <avner@hey.com>
2024-05-28 18:16:12 -0500

    Cut a new release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description():
 setup(
     name='visible-to-sqlite',
     author='Avner Shanan',
-    version='0.1.0',
+    version='1.0.0',
     description="Convert exported CSV from Visible app to a SQLite DB",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
