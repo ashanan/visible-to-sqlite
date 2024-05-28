2e41e1b3a52dc7099c39567410d9eb8df944718f
Avner Shanan <avner@hey.com>
2024-05-28 16:45:06 -0500

    Add test

--- test/test_visible_to_sqlite.py
+++ test/test_visible_to_sqlite.py
@@ -1,6 +1,7 @@
 import csv
 import pathlib
 import pytest
+from io import StringIO
 from sqlite_utils import Database
 from visible_to_sqlite import utils
 
@@ -13,9 +14,21 @@ def db():
 def csv_path():
     return pathlib.Path(__file__).parent / "export.csv"
 
+@pytest.fixture
+def single_row_export_csv():
+    header = 'observation_date,tracker_name,tracker_category,observation_value \n'
+    row = '2024-05-22,Sleep,Sleep,2 '
+    return StringIO(header + row)
+
 def test_tables_created(csv_path, db):
     with open(csv_path, newline='') as csvfile:
         csvreader = csv.DictReader(csvfile)
         utils.convert_csv_to_sqlite(csvreader, db)
 
     assert db.table_names() == ['TrackerCategories', 'Trackers', 'Observations']
+
+def test_upsert_creates_new_rows_when_no_match_exists(single_row_export_csv, db):
+    csvreader = csv.DictReader(single_row_export_csv)
+    utils.convert_csv_to_sqlite(csvreader, db)
+
+    assert db.table("Observations").count == 1
